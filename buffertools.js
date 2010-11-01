1d429cc13b0dc298a639d80d92db17924da04443
Nathan Rajlich <nathan@tootallnate.net>
2010-11-01 16:39:24 -0700

    Export 'static' versions of each function on the module itself.
    This allows the 'this' Buffer instance to be passed as the first argument, for
    when you would rather use not use the prototype extended methods.

--- buffertools.js
+++ buffertools.js
@@ -1,8 +1,22 @@
-buffertools = require('./buffertools.node');
-SlowBuffer = require('buffer').SlowBuffer;
-Buffer = require('buffer').Buffer;
+var buffertools = require('./buffertools.node');
+var SlowBuffer = require('buffer').SlowBuffer;
+var Buffer = require('buffer').Buffer;
+
+var slice = Array.prototype.slice;
 
 // extend object prototypes
-for (var property in buffertools) {
-	exports[property] = Buffer.prototype[property] = SlowBuffer.prototype[property] = buffertools[property];
-}
+Object.keys(buffertools).forEach(function(property) {
+
+  // the globally extended 'prototype's
+  // TODO: figure out a way to do this on a per-module basis
+  Buffer.prototype[property] = SlowBuffer.prototype[property] = buffertools[property];
+
+  // export a 'static' version of the helper function, where the
+  // 'this' Buffer is passed as the first argument instead
+  exports[property] = function() {
+    var buffer = arguments[0];
+    var args = slice.call(arguments, 1);
+    return buffertools[property].apply(buffer, args);
+  }
+
+});
