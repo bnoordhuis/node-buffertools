ca2004c2673a5a01786077c901b3467acf4842eb
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2016-02-13 20:33:14 +0100

    Signed char for negative values

--- buffertools.cc
+++ buffertools.cc
@@ -344,7 +344,7 @@ struct IndexOfAction: BinaryAction<IndexOfAction> {
 static char toHexTable[] = "0123456789abcdef";
 
 // CHECKME is this cache efficient?
-static char fromHexTable[] = {
+static signed char fromHexTable[] = {
     -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
     -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,0,1,2,3,4,5,6,7,8,9,-1,-1,-1,-1,-1,-1,-1,
     10,11,12,13,14,15,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
